169f4ceb7d28bb466ff0c61577b0e68df37160a2
Atharvpatil322 <127306088+Atharvpatil322@users.noreply.github.com>
2024-03-02 18:30:49 +0530

    Update listings.js

--- controllers/listings.js
+++ controllers/listings.js
@@ -45,9 +45,7 @@ module.exports.renderNewForm = (req,res) => {
 module.exports.showListing = async (req,res) => {
     let {id} = req.params;
     // .populate("reviews", "users") -> As we stored reviewId in ListingSchema and we neeed their data. so we'll use this method
-    // here we are using nested populate (using path) for reviews to access the author, as we need the author's id info. As in reviewSchema we're storing just the author's Id and not its info so inorder to access the info we use .populate.
-    // for every individual we need author in our path.
-    // this means for every review we populate its (that specific review's) author.
+
     const listing = await Listing.findById(id).populate({path : "reviews", populate : {path : "author"}}).populate("owner");
     if(!listing){
       req.flash("error", "Listing you requested for does not exist");
@@ -72,12 +70,8 @@ module.exports.createListing = async (req, res, next) => {
     let filename = req.file.filename;
 
     const newListing = new Listing(req.body.listing);
-    // here the new listing that is created...we are storing our current owner id in it from req.user.
     newListing.owner =  req.user._id;
-    // now when we create a newListing, here we add url and filename (that we extracted)in our newListing.image
     newListing.image = {url,  filename};
-
-    // here response.body contains the coordinates of the place which we are storing in our newlisitng.
     // (response.body.featuress[0].geometry) its the path to get those coordinates.
     newListing.geometry = response.body.features[0].geometry;
 
@@ -104,13 +98,10 @@ module.exports.updateListing =  async (req, res) => {
     let { id } = req.params;
     let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
 
-    // The typeof operator in JavaScript is used to find the type of a variable, function, or object.
-    // if req.file is not undefined then execute the code (ie. extract the url, filename from req.file and save the new listing).
-    // This logic is used if by any chance the "update image" is empty.
     if(typeof req.file !== "undefined"){
       let url = req.file.path;
       let filename = req.file.filename;
-      // now when we updated our listing, here we add url and filename (that we extracted)in our listing.image
+ 
       listing.image = { url, filename};
       await listing.save();
     };
